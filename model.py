aedd1e6a24f627d9258e1494d3974340808d9117
P.Rajeswari Medini <146118092+medinirajeswari@users.noreply.github.com>
2025-01-04 13:41:22 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -6,30 +7,30 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
-# Data Loading
+
 file_path = 'Inventory_DataSet.xlsx'
 data = pd.read_excel(file_path, sheet_name='Product', engine='openpyxl')
 
-# Data preprocessing
+
 features = ['Product Cateogy Name', 'Model Number', 'Supplier Name', 'Order Date']
 targets = ['StockLevel', 'ReorderPoint', 'Quantity']
 
-# Handle the categorical variables
+
 encoder = LabelEncoder()
 for col in ['Product Cateogy Name', 'Model Number', 'Supplier Name']:
     data[col] = encoder.fit_transform(data[col].astype(str))
 
-# Changing the date format into numerical and removed invalid dates
+
 data['Order Date'] = pd.to_datetime(data['Order Date'], errors='coerce')
 
-# Replaced the invalid dates from data with the mean or a default values
+
 mean_date = data['Order Date'].dropna().mean()
 data['Order Date'].fillna(mean_date, inplace=True)
 
-# Dates are converted into numerical Timestamp
+
 data['Order Date'] = data['Order Date'].astype('int64') // 10**9
 
-# Time-series data preparation
+
 data = data.sort_values(by='Order Date')
 
 def create_sequences(data, target_columns, sequence_length):
@@ -44,7 +45,7 @@ def create_sequences(data, target_columns, sequence_length):
 
     return np.array(sequences), np.array(targets)
 
-# Sequence length
+
 sequence_length = 10  
 
 X, y = create_sequences(data, targets, sequence_length)
@@ -57,7 +58,7 @@ X_test = X_test.reshape(-1, X_test.shape[-1])
 X_train = scaler.fit_transform(X_train).reshape(-1, sequence_length, len(features))
 X_test = scaler.transform(X_test).reshape(-1, sequence_length, len(features))
 
-# Model Building
+
 model = Sequential()
 model.add(LSTM(128, activation='relu', return_sequences=True, input_shape=(sequence_length, len(features))))
 model.add(Dropout(0.2))
@@ -74,7 +75,7 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr
 y_train = y_train.astype(np.float32)
 y_test = y_test.astype(np.float32)
 
-# Model training
+
 history = model.fit(
     X_train,
     y_train,  
